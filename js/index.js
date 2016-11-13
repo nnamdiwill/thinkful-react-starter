ea46e5992af7ba9fe6b624836f8f42ad0b597d06
BuzznBias <nnamdiwill@gmail.com>
2016-11-13 23:14:55 +0000

    complete redux project

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
 require('babel-polyfill');
 
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var RepositoryList = require('./components/repository-list');
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(<Provider store={store}>
+            <RepositoryList />
+        </Provider>, document.getElementById('app'));
+});
